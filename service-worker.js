e231145c50be77b2d43b1bae5684d9005cee960f
VinicinDosCodis <vinimano883@gmail.com>
2025-11-19 17:04:13 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,20 +1,21 @@
-self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open("catcam-cache").then(cache => {
-      return cache.addAll([
-        "index.html",
-        "style.css",
-        "app.js",
-        "manifest.json",
-        "icon-192.png",
-        "icon-512.png"
-      ]);
-    })
-  );
+self.addEventListener("install", (e) => {
+    e.waitUntil(
+        caches.open("catcam-v1").then((cache) => {
+            return cache.addAll([
+                "/",
+                "index.html",
+                "style.css",
+                "app.js",
+                "manifest.json",
+                "icons/icon-192.png",
+                "icons/icon-512.png"
+            ]);
+        })
+    );
 });
 
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request).then(resp => resp || fetch(event.request))
-  );
+self.addEventListener("fetch", (e) => {
+    e.respondWith(
+        caches.match(e.request).then((resp) => resp || fetch(e.request))
+    );
 });
