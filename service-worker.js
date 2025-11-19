ebc97449c5b5cd57cffa7016cd8fd402cc93b7ba
VinicinDosCodis <vinimano883@gmail.com>
2025-11-19 16:29:20 -0300

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,1 @@
+self.addEventListener('install',e=>{e.waitUntil(caches.open('catcam-cache').then(c=>c.addAll(['index.html','style.css','app.js','manifest.json','icon-192.png','icon-512.png'])))});self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))))};
